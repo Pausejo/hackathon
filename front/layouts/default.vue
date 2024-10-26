d5f251fb4ce83cfb23b311c331a1cdc9ab869804
Pablo Ausejo <pausejo@hotmail.com>
2024-10-26 12:38:43 +0200

    Add default layout

---
+++ front/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Agents configurator</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item title="Home" to="/" prepend-icon="mdi-home" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+const drawer = ref(true);
+</script>
